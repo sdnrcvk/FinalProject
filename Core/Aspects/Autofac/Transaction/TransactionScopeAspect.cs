45170c5a48209b1f9ffcf6c5f0454f233341c249
Sedanur Çevik <sdnrcvk@gmail.com>
2021-03-11 02:04:45 +0300

    UseCors/Thread.Sleep

--- Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
+++ Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -18,7 +18,7 @@ namespace Core.Aspects.Autofac.Transaction
                     invocation.Proceed();
                     transactionScope.Complete();
                 }
-                catch (System.Exception)
+                catch (System.Exception e)
                 {
                     transactionScope.Dispose();
                     throw;
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -45,6 +45,7 @@ namespace WebAPI
             // services.addsingleton<ýproductservice,productmanager>();
             // services.addsingleton<ýproductdal, efproductdal>();
 
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -74,6 +75,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
